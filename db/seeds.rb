fa9711f58f3bcb3e346b2489a4fe63d5ba4f736b
Ranjae <ranjaebrilliant21@gmail.com>
2022-12-05 10:24:03 +0300

    created the seed data for the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+puts "🌱 Seeding Greatness!..."
+
+#Seeds for Users
+user1 = User.create(username: "skunkhunt42", email: "skunkhunt@gmail.com", password_digest: "lowkey4ever")
+user2 = User.create(username: "thanh_mclaughin", email: "thanh@hotmail.com", password_digest: "loveliveserve")
+user3 = User.create(username: "simona_bednar", email: "simonabednar@yahoo.com", password_digest: "Konichi254@wa2")
+user4 = User.create(username: "stefany", email: "stefany@gmail.com", password_digest: "streetsmart")
+user5 = User.create(username: "emmanuel", email: "emmanuel54@hotmail.com", password_digest: "comegetit254")
+user6 = User.create(username: "lecia_feil", email: "leciafeil23@gmail.com", password_digest: "neverhadachance")
+user7 = User.create(username: "felipe.buckbridge", email: "felipebuckbridge33@gmail.com", password_digest: "neverwakalone")
+user8 = User.create(username: "pasty", email: "pasty34@hotmail.com", password_digest: "inthestreets4eva")
+user9 = User.create(username: "hertha", email: "hertha55@hotmail.com", password_digest: "droptop6654")
+user10 = User.create(username: "TravisScott", email: "travisscott999@gmail.com", password_digest: "metrobooominwantsomemore")
+user11 = User.create(username: "gale", email: "gale370@gmail.com", password_digest: "comegerrit")
+user12 = User.create(username: "damian", email: "damianvirgo@gmail.com", password_digest: "123herewego")
+user13 = User.create(username: "lindsey sterling", email: "lindseysterling@yahoo.com", password_digest: "comewego")
+user14 = User.create(username: "duraglord", email: "duraglord@hotmail.com", password_digest: "loseyounow")
+user15 = User.create(username: "westsideniccur", email: "westsideniccur@yahoo.com", password_digest: "highkey4ever")
+#Seeds for Food
+food1 = Food.create(name: "Pizza", price: "3238", food_url: "https://unsplash.com/photos/MQUqbmszGGM", description: "crispy", review: "The pizza was crispy and tasty")
+food2 = Food.create(name: "French Toast", price: "585", food_url: "https://unsplash.com/photos/SQ20tWzxXO0", description: "delicious", review: "the french toast was fresh and delicious")
+food3 = Food.create(name: "Fish and Chips", price: "300", food_url: "https://unsplash.com/photos/zWdBRmh8Gv8", description: "fresh", review: "fish and chips were fresh and quite adequate for one's lunch")
+food4 = Food.create(name: "Meatball with Sauce", price: "820", food_url: "https://unsplash.com/photos/AUAuEgUxg5Q", description: "juicy", review: "meatballs had a nice crust and the sauce was juicy as well as having a conspicuous taste")
+food5 = Food.create(name: "Scotch Eggs", price: "150", food_url: "https://unsplash.com/photos/efNvQRK-QBg", description: "hot", review: "the scotch eggs were hot and to add on they had a fine finish")
+food6 = Food.create(name: "Chicken Wings", price: "350", food_url: "https://unsplash.com/photos/MQUqbmszGGM", description: "delicious", review: "the chicken wings were well fried and they also had a fine finish")
+food7 = Food.create(name: "Barbecue Ribs", price: "238", food_url: "https://unsplash.com/photos/5nbMVj7DLPk", description: "juicy", review: "the ribs not only had a fine finish but they also had a juicy taste which will leave you wanting more")
+food8 = Food.create(name: "Ricotta Stuffed Ravioli", price: "450", food_url: "https://unsplash.com/photos/hQckRlItq4c", description: "creamy", review: "they not only had a fine finish but they also was adequate for someone's meal")
+food9 = Food.create(name: "Pasta Cabonara", price: "427", food_url: "https://unsplash.com/photos/PLyJqEJVre0", description: "delicious", review: "the pasta ravioli was fine with it's cream which will leave you wanting more")
+food10 = Food.create(name: "Philadelphia Maki", price: "187", food_url: "https://unsplash.com/photos/4UFjmTOtVnU", description: "delicious", review: "this was affordable and adequate for one's meal and eanough for kids too")
+food11 = Food.create(name: "Chicken Tikka", price: "320", food_url: "https://unsplash.com/photos/UC0HZdUitWY", description: "crispy", review: "the chicken was quite crispy in a good way and the stew was quite delicous and also had some nice flavour with it")
+food12 = Food.create(name: "Lamb Biryani", price: "530", food_url: "https://unsplash.com/photos/jkgbZXv0nm0", description: "delicious", review: "this is mostly a family meal and it has also nice flavour with it")
+
+puts "✅ Done seeding!"
